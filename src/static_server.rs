769fde278d831468af7365f4e093c8d30a149bab
Daniil Pryima <swimmin2@gmail.com>
2025-02-03 01:59:33 +0200

    net: Add remote banners and webviews

--- src/static_server.rs
+++ src/static_server.rs
@@ -1,5 +1,10 @@
+use std::convert::Infallible;
+use std::future::Future;
 use std::io;
+use std::pin::Pin;
+use std::task::{Context, Poll};
 
+use anyhow::anyhow;
 use axum::body::{Body, Bytes};
 use axum::extract::{MatchedPath, Path, Request};
 use axum::handler::HandlerWithoutStateExt;
@@ -10,7 +15,7 @@ use axum::routing::get;
 use axum::{Json, Router, ServiceExt};
 use reqwest::Client;
 use serde::Serialize;
-use tower::Layer;
+use tower::{Layer, Service};
 use tower_http::services::ServeDir;
 use tower_http::set_status::SetStatus;
 use tower_http::trace::TraceLayer;
@@ -25,7 +30,21 @@ pub async fn start() -> io::Result<()> {
     .route("/versions/:version", get(get_version))
     .nest_service(
       "/bundles",
-      ServeDir::new("static/bundles").fallback(get_bundle_remote.into_service()),
+      ServeDir::new("static/bundles").fallback(ServeRemoteResource::new(
+        "https://smb.assasans.dev/konofd/bundles/".parse().unwrap(),
+      )),
+    )
+    .nest_service(
+      "/banners",
+      ServeDir::new("static/banners").fallback(ServeRemoteResource::new(
+        "https://smb.assasans.dev/konofd/banners/".parse().unwrap(),
+      )),
+    )
+    .nest_service(
+      "/webview",
+      ServeDir::new("static/webview").fallback(ServeRemoteResource::new(
+        "https://smb.assasans.dev/konofd/webview/".parse().unwrap(),
+      )),
     )
     .layer(
       TraceLayer::new_for_http()
@@ -55,38 +74,59 @@ pub async fn start() -> io::Result<()> {
   axum::serve(listener, app.into_make_service()).await
 }
 
-const REMOTE_URL: &str = "https://smb.assasans.dev/konofd/bundles/";
+#[derive(Debug, Clone)]
+struct ServeRemoteResource {
+  remote_url: Url,
+}
 
-async fn get_bundle_remote(request: Request) -> axum::response::Result<impl IntoResponse, AppError> {
-  let path = &request.uri().path()[1..];
-  let remote_url = Url::parse(REMOTE_URL).unwrap().join(path).unwrap();
-  info!("get bundle remote: {} -> {}", path, remote_url);
+impl ServeRemoteResource {
+  pub fn new(remote_url: Url) -> Self {
+    Self { remote_url }
+  }
+}
 
-  let client = Client::new();
-  let response = client.get(remote_url).send().await.unwrap();
+impl Service<Request> for ServeRemoteResource {
+  type Response = Response;
+  type Error = Infallible;
+  type Future = Pin<Box<dyn Future<Output = Result<Self::Response, Self::Error>> + Send + Sync + 'static>>;
 
-  let status = StatusCode::from_u16(response.status().as_u16()).unwrap_or(StatusCode::INTERNAL_SERVER_ERROR);
-
-  let headers = response.headers().clone();
-  let stream = response.bytes_stream();
-  let body = Body::from_stream(stream);
-
-  let mut builder = Builder::new().status(status);
-  for (name, value) in headers {
-    let name = match name {
-      Some(name) => name,
-      None => continue,
-    };
-
-    builder = builder.header(name, value);
+  fn poll_ready(&mut self, cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
+    Poll::Ready(Ok(()))
   }
 
-  Ok(builder.body(body).unwrap_or_else(|_| {
-    Response::builder()
-      .status(StatusCode::INTERNAL_SERVER_ERROR)
-      .body(Body::empty())
-      .unwrap()
-  }))
+  fn call(&mut self, request: Request) -> Self::Future {
+    let path = &request.uri().path()[1..];
+    let remote_url = self.remote_url.join(path).unwrap();
+    info!("get bundle remote: {} -> {}", path, remote_url);
+
+    Box::pin(async {
+      let client = Client::new();
+      let response = client.get(remote_url).send().await.unwrap();
+
+      let status = StatusCode::from_u16(response.status().as_u16()).unwrap_or(StatusCode::INTERNAL_SERVER_ERROR);
+
+      let headers = response.headers().clone();
+      let stream = response.bytes_stream();
+      let body = Body::from_stream(stream);
+
+      let mut builder = Builder::new().status(status);
+      for (name, value) in headers {
+        let name = match name {
+          Some(name) => name,
+          None => continue,
+        };
+
+        builder = builder.header(name, value);
+      }
+
+      Ok(builder.body(body).unwrap_or_else(|_| {
+        Response::builder()
+          .status(StatusCode::INTERNAL_SERVER_ERROR)
+          .body(Body::empty())
+          .unwrap()
+      }))
+    })
+  }
 }
 
 async fn get_version(Path(version): Path<String>) -> axum::response::Result<impl IntoResponse, AppError> {
