Assasans/axel

commit f6b3b068f96fcc0f03ca93a892ea5ec0608214ee

db: Store characters and members
Don't know what needs to be stored and what is computed.

src/api/mod.rs
@@ -227,9 +227,9 @@ impl ActiveSkill {
 pub struct MemberStats {
   pub hp: i32,
   pub attack: i32,
-  pub magicattack: i32,
+  pub attack_magic: i32,
   pub defense: i32,
-  pub magicdefence: i32,
+  pub defense_magic: i32,
   pub agility: i32,
   pub dexterity: i32,
   pub luck: i32,

src/blob.rs
@@ -1,13 +1,104 @@
 use crate::api::master_all::get_master_manager;
-use crate::api::{CharacterParameter, MemberParameterWire, RemoteData, RemoteDataCommand, RemoteDataItemType, SpSkill};
-use crate::member::MemberPrototype;
+use crate::api::{
+  CharacterParameter, MemberFameStats, MemberParameterWire, MemberStats, RemoteData, RemoteDataCommand,
+  RemoteDataItemType, SpSkill,
+};
+use crate::level::get_member_level_calculator;
+use crate::member::{Member, MemberActiveSkill, MemberPrototype, MemberStrength};
+use crate::user::session::Session;
+use crate::AppState;
+use anyhow::Context;
+use std::sync::Arc;
+use tracing::info;
 
-pub async fn get_login_remote_data() -> Vec<RemoteData> {
+pub async fn get_login_remote_data(state: &AppState, session: &Session) -> Vec<RemoteData> {
   let masters = get_master_manager();
   let characters = masters.get_master("character");
-  let members = masters.get_master("member");
+  // let members = masters.get_master("member");
   let costumes = masters.get_master("costume");
   let backgrounds = masters.get_master("background");
+
+  let client = state
+    .pool
+    .get()
+    .await
+    .context("failed to get database connection")
+    .unwrap();
+  #[rustfmt::skip]
+  let statement = client
+    .prepare(/* language=postgresql */ r#"
+      select
+        member_id,
+        xp,
+        promotion_level
+      from user_members
+      where user_id = $1
+    "#)
+    .await
+    .context("failed to prepare statement").unwrap();
+  let rows = client
+    .query(&statement, &[&session.user_id])
+    .await
+    .context("failed to execute query")
+    .unwrap();
+  info!(?rows, "get friend info query executed");
+
+  let members = rows
+    .iter()
+    .enumerate()
+    .map(|(index, row)| {
+      let member_id: i64 = row.get(0);
+      let xp: i32 = row.get(1);
+      let promotion_level: i32 = row.get(2);
+      // let active_skills: Value = row.get(3);
+      let prototype = MemberPrototype::load_from_id(member_id);
+
+      Member {
+        id: index as i32 + 1,
+        prototype: &prototype,
+        xp,
+        promotion_level,
+        active_skills: prototype
+          .active_skills
+          .iter()
+          .map(|skill_opt| {
+            skill_opt.as_ref().map(|skill| MemberActiveSkill {
+              prototype: skill,
+              level: 1,
+              value: skill.value.max,
+            })
+          })
+          .collect::<Vec<_>>()
+          .try_into()
+          .unwrap(),
+        // active_skills: prototype
+        //   .active_skills
+        //   .iter()
+        //   .enumerate()
+        //   .map(|(index, prototype)| {
+        //     // TODO: Wrong
+        //     let active_skill = active_skills.get(index).unwrap();
+        //     // let skill_id = active_skill["id"].as_i64().unwrap();
+        //     let level = active_skill["level"].as_i64().unwrap() as i32;
+        //     let value = active_skill["value"].as_i64().unwrap() as i32;
+        //     Some(MemberActiveSkill {
+        //       prototype: &prototype,
+        //       level,
+        //       value,
+        //     })
+        //   })
+        //   .try_into()
+        //   .unwrap(),
+        stats: prototype.stats.clone(),
+        main_strength: MemberStrength::default(),
+        sub_strength: MemberStrength::default(),
+        sub_strength_bonus: MemberStrength::default(),
+        fame_stats: MemberFameStats::default(),
+        skill_pa_fame_list: vec![],
+      }
+      .to_member_parameter_wire()
+    })
+    .collect::<Vec<_>>();
 
   let characters = characters
     .iter()
@@ -60,17 +151,9 @@ pub async fn get_login_remote_data() -> Vec<RemoteData> {
     .collect::<Vec<_>>();
 
   let members = members
-    .iter()
+    .into_iter()
     .enumerate()
-    .map(|(index, member)| {
-      AddMember::new(
-        MemberPrototype::load_from_id(member["id"].as_str().unwrap().parse::<i64>().unwrap())
-          .create_member(index as i32 + 1)
-          .to_member_parameter_wire(),
-        "front",
-      )
-      .into_remote_data()
-    })
+    .map(|(index, member)| AddMember::new(member, "front").into_remote_data())
     .collect::<Vec<_>>();
 
   #[cfg_attr(rustfmt, rustfmt::skip)]

src/level/member.rs
@@ -7,14 +7,18 @@ pub struct MemberLevelCalculator {
   absolute_xp_to_level: BTreeMap<i32, BTreeMap<i32, i32>>,
   // (rarity, (level, xp))
   level_to_absolute_xp: BTreeMap<i32, BTreeMap<i32, i32>>,
+  // (promotion_level, cumulative{bonus_levels))
+  promotion_levels: BTreeMap<i32, i32>,
 }
 
 impl MemberLevelCalculator {
   fn new() -> Self {
     let rarities = [1, 2, 3, 4];
-    let mut levels = Vec::new();
+    let mut absolute_xp_to_level = BTreeMap::new();
+    let mut level_to_absolute_xp = BTreeMap::new();
     for rarity in rarities {
-      let mut levels_for_rarity = Vec::new();
+      let mut xp_to_level = BTreeMap::new();
+      let mut level_to_xp = BTreeMap::new();
       for entry in get_master_manager().get_master("member_lv_exp") {
         let level: i32 = entry["lv"].as_str().unwrap().parse().unwrap();
         let xp: i32 = entry[&format!("exp_rare_{}", rarity)]
@@ -22,51 +26,64 @@ impl MemberLevelCalculator {
           .unwrap()
           .parse()
           .unwrap();
-        levels_for_rarity.push((level, xp));
+        xp_to_level.insert(xp, level);
+        level_to_xp.insert(level, xp);
       }
-      levels.push((rarity, levels_for_rarity));
+      absolute_xp_to_level.insert(rarity, xp_to_level);
+      level_to_absolute_xp.insert(rarity, level_to_xp);
     }
-    Self::from_levels(&levels)
-  }
 
-  pub fn from_levels(levels: &[(i32, Vec<(i32, i32)>)]) -> Self {
-    let mut absolute_xp_to_level = BTreeMap::new();
-    let mut level_to_absolute_xp = BTreeMap::new();
-    for (rarity, levels_for_rarity) in levels {
-      let mut absolute_xp_to_level_local = BTreeMap::new();
-      let mut level_to_absolute_xp_local = BTreeMap::new();
-      for &(level, required_exp) in levels_for_rarity {
-        absolute_xp_to_level_local.insert(required_exp, level);
-        level_to_absolute_xp_local.insert(level, required_exp);
-      }
-
-      absolute_xp_to_level.insert(*rarity, absolute_xp_to_level_local);
-      level_to_absolute_xp.insert(*rarity, level_to_absolute_xp_local);
+    let mut promotion_levels = BTreeMap::new();
+    let mut cumulative = 0;
+    for promotion_entry in get_master_manager().get_master("member_lv_limitbreak") {
+      let promotion_level: i32 = promotion_entry["lv"].as_str().unwrap().parse().unwrap();
+      let bonus_levels: i32 = promotion_entry["lv_limit"].as_str().unwrap().parse().unwrap();
+      cumulative += bonus_levels;
+      promotion_levels.insert(promotion_level, cumulative);
     }
 
     Self {
       absolute_xp_to_level,
       level_to_absolute_xp,
+      promotion_levels,
     }
   }
 
-  pub fn get_level(&self, rarity: i32, exp: i32) -> i32 {
+  pub fn get_level(&self, xp: i32, rarity: i32, promotion_level: i32) -> i32 {
     let absolute_xp_to_level_local = match self.absolute_xp_to_level.get(&rarity) {
       Some(map) => map,
       None => todo!("handle unknown rarity {}", rarity),
     };
-    match absolute_xp_to_level_local.range(..=exp).next_back() {
+
+    let base_level = match absolute_xp_to_level_local.range(..=xp).next_back() {
       Some((_, &level)) => level,
+      // 'member_lv_exp' starts from level 2, so this branch is actually reachable
+      None => 1,
+    };
+
+    // Not sure if .range() is needed here, 'member_lv_limitbreak' has no gaps
+    let bonus_levels = match self.promotion_levels.range(..=promotion_level).next_back() {
+      Some((_, &levels)) => levels,
+      // promotion_level is 0
       None => 0,
-    }
+    };
+
+    base_level + bonus_levels
   }
 
-  pub fn get_xp_for_level(&self, level: i32) -> Option<i32> {
-    let level_to_absolute_xp_local = match self.level_to_absolute_xp.get(&level) {
+  /// Returns the total XP required to reach the given level for a member of the given rarity,
+  /// ignoring level caps.
+  pub fn get_xp_for_level(&self, level: i32, rarity: i32) -> i32 {
+    let level_to_absolute_xp_local = match self.level_to_absolute_xp.get(&rarity) {
       Some(map) => map,
-      None => return None,
+      None => todo!("handle unknown rarity {}", rarity),
     };
-    level_to_absolute_xp_local.get(&level).copied()
+
+    match level_to_absolute_xp_local.range(..=level).next_back() {
+      Some((_, &xp)) => xp,
+      // 'member_lv_exp' starts from level 2, so this branch is actually reachable
+      None => 0,
+    }
   }
 }
 

src/member.rs
@@ -1,3 +1,4 @@
+use crate::api::battle::BattleMember;
 use crate::api::dungeon::PartyMember;
 use crate::api::master_all::get_master_manager;
 use crate::api::{MemberFameStats, MemberParameterWire, MemberStats, SkillPaFame};
@@ -80,7 +81,7 @@ impl MemberPrototype {
         member["min_attack"].as_str().unwrap().parse::<i32>().unwrap(),
         member["max_attack"].as_str().unwrap().parse::<i32>().unwrap(),
       ),
-      magic_attack: MinMaxRange::new(
+      attack_magic: MinMaxRange::new(
         member["min_magicattak"].as_str().unwrap().parse::<i32>().unwrap(),
         member["max_magicattak"].as_str().unwrap().parse::<i32>().unwrap(),
       ),
@@ -88,7 +89,7 @@ impl MemberPrototype {
         member["min_defense"].as_str().unwrap().parse::<i32>().unwrap(),
         member["max_defense"].as_str().unwrap().parse::<i32>().unwrap(),
       ),
-      magic_defense: MinMaxRange::new(
+      defense_magic: MinMaxRange::new(
         member["min_magicdefence"].as_str().unwrap().parse::<i32>().unwrap(),
         member["max_magicdefence"].as_str().unwrap().parse::<i32>().unwrap(),
       ),
@@ -123,7 +124,8 @@ impl MemberPrototype {
     Member {
       id,
       prototype: self,
-      xp: if self.character_id == 102 { 150_000 } else { 35_000 },
+      xp: if self.character_id == 102 { 150_000 } else { 35_000 } * 20,
+      promotion_level: 2,
       active_skills: self
         .active_skills
         .iter()
@@ -137,18 +139,7 @@ impl MemberPrototype {
         .collect::<Vec<_>>()
         .try_into()
         .unwrap(),
-      stats: MemberStats {
-        hp: self.stats.hp.max,
-        attack: self.stats.attack.max,
-        magicattack: self.stats.magic_attack.max,
-        defense: self.stats.defense.max,
-        magicdefence: self.stats.magic_defense.max,
-        agility: self.stats.agility.max,
-        dexterity: self.stats.dexterity.max,
-        luck: self.stats.luck.max,
-      },
-      limit_break: 2,
-      waiting_room: 0,
+      stats: self.stats.clone(),
       main_strength: MemberStrength::default(),
       sub_strength: MemberStrength::default(),
       sub_strength_bonus: MemberStrength::default(),
@@ -170,11 +161,10 @@ pub struct Member<'a> {
   pub id: i32,
   pub prototype: &'a MemberPrototype,
   pub xp: i32,
-  pub active_skills: [Option<MemberActiveSkill<'a>>; 3],
-  pub stats: MemberStats,
   /// "Promotions"
-  pub limit_break: i32,
-  pub waiting_room: i32,
+  pub promotion_level: i32,
+  pub active_skills: [Option<MemberActiveSkill<'a>>; 3],
+  pub stats: MemberStatsPrototype,
   pub main_strength: MemberStrength,
   pub sub_strength: MemberStrength,
   pub sub_strength_bonus: MemberStrength,
@@ -183,8 +173,15 @@ pub struct Member<'a> {
 }
 
 impl Member<'_> {
+  /// ## Level
+  /// Members have level cap ('member_lv_limit' master data, currently level 30 for all)
+  /// based on their rarity.
+  ///
+  /// ## Promotion
+  /// Called 'limit_break' internally, promotions increase the level cap of a member
+  /// ('member_lv_limitbreak' master data, currently 5 promotions resulting in 30 bonus levels).
   pub fn level(&self) -> i32 {
-    get_member_level_calculator().get_level(self.prototype.rarity, self.xp)
+    get_member_level_calculator().get_level(self.xp, self.prototype.rarity, self.promotion_level)
   }
 
   pub fn to_member_parameter_wire(&self) -> MemberParameterWire {
@@ -202,21 +199,21 @@ impl Member<'_> {
       ac_skill_id_c: self.active_skills[2].as_ref().map_or(0, |skill| skill.prototype.id),
       ac_skill_lv_c: self.active_skills[2].as_ref().map_or(0, |skill| skill.level),
       ac_skill_val_c: self.active_skills[2].as_ref().map_or(0, |skill| skill.value),
-      hp: self.stats.hp,
-      magicattack: self.stats.magicattack,
-      defense: self.stats.defense,
-      magicdefence: self.stats.magicdefence,
-      agility: self.stats.agility,
-      dexterity: self.stats.dexterity,
-      luck: self.stats.luck,
-      limit_break: self.limit_break,
+      hp: self.stats.hp.interpolate(self.level()),
+      magicattack: self.stats.attack_magic.interpolate(self.level()),
+      defense: self.stats.defense.interpolate(self.level()),
+      magicdefence: self.stats.defense_magic.interpolate(self.level()),
+      agility: self.stats.agility.interpolate(self.level()),
+      dexterity: self.stats.dexterity.interpolate(self.level()),
+      luck: self.stats.luck.interpolate(self.level()),
+      limit_break: self.promotion_level,
       character_id: self.prototype.character_id,
       passiveskill: self.prototype.passive_skill.as_ref().map_or(0, |skill| skill.id),
       specialattack: self.prototype.special_attack.as_ref().map_or(0, |skill| skill.id),
       resist_state: self.prototype.resistance_group.id,
       resist_attr: 0,
-      attack: self.stats.attack,
-      waiting_room: self.waiting_room,
+      attack: self.stats.attack.interpolate(self.level()),
+      waiting_room: 0,
       main_strength: self.main_strength.strength,
       main_strength_for_fame_quest: self.main_strength.for_fame_quest,
       sub_strength: self.sub_strength.strength,
@@ -244,19 +241,54 @@ impl Member<'_> {
       ac_skill_val_b: self.active_skills[1].as_ref().map_or(0, |skill| skill.value as i64),
       ac_skill_lv_c: self.active_skills[2].as_ref().map_or(0, |skill| skill.level),
       ac_skill_val_c: self.active_skills[2].as_ref().map_or(0, |skill| skill.value as i64),
-      hp: self.stats.hp,
-      attack: self.stats.attack,
-      magicattack: self.stats.magicattack,
-      defense: self.stats.defense,
-      magicdefence: self.stats.magicdefence,
-      agility: self.stats.agility,
-      dexterity: self.stats.dexterity,
-      luck: self.stats.luck,
-      limit_break: self.limit_break,
+      hp: self.stats.hp.interpolate(self.level()),
+      attack: self.stats.attack.interpolate(self.level()),
+      magicattack: self.stats.attack_magic.interpolate(self.level()),
+      defense: self.stats.defense.interpolate(self.level()),
+      magicdefence: self.stats.defense_magic.interpolate(self.level()),
+      agility: self.stats.agility.interpolate(self.level()),
+      dexterity: self.stats.dexterity.interpolate(self.level()),
+      luck: self.stats.luck.interpolate(self.level()),
+      limit_break: self.promotion_level,
       character_id: self.prototype.character_id,
-      waiting_room: self.waiting_room,
+      waiting_room: 0,
       ex_flg: 0,
       is_undead: 0,
+    }
+  }
+
+  pub fn to_battle_member(&self) -> BattleMember {
+    BattleMember {
+      id: self.id,
+      lv: self.level(),
+      exp: self.xp,
+      member_id: self.prototype.id,
+      ac_skill_id_a: self.active_skills[0].as_ref().map_or(0, |skill| skill.prototype.id),
+      ac_skill_lv_a: self.active_skills[0].as_ref().map_or(0, |skill| skill.level),
+      ac_skill_val_a: self.active_skills[0].as_ref().map_or(0, |skill| skill.value),
+      ac_skill_id_b: self.active_skills[1].as_ref().map_or(0, |skill| skill.prototype.id),
+      ac_skill_lv_b: self.active_skills[1].as_ref().map_or(0, |skill| skill.level),
+      ac_skill_val_b: self.active_skills[1].as_ref().map_or(0, |skill| skill.value),
+      ac_skill_id_c: self.active_skills[2].as_ref().map_or(0, |skill| skill.prototype.id),
+      ac_skill_lv_c: self.active_skills[2].as_ref().map_or(0, |skill| skill.level),
+      ac_skill_val_c: self.active_skills[2].as_ref().map_or(0, |skill| skill.value),
+      hp: self.stats.hp.interpolate(self.level()),
+      magicattack: self.stats.attack_magic.interpolate(self.level()),
+      defense: self.stats.defense.interpolate(self.level()),
+      magicdefence: self.stats.defense_magic.interpolate(self.level()),
+      agility: self.stats.agility.interpolate(self.level()),
+      dexterity: self.stats.dexterity.interpolate(self.level()),
+      luck: self.stats.luck.interpolate(self.level()),
+      limit_break: self.promotion_level,
+      character_id: self.prototype.character_id,
+      passiveskill: 210201,  // self.prototype.passive_skill.as_ref().map_or(0, |skill| skill.id),
+      specialattack: 100001, // self.prototype.special_attack.as_ref().map_or(0, |skill| skill.id),
+      resist_state: 210201,  // self.prototype.resistance_group.id,
+      resist_attr: 150000000,
+      attack: self.stats.attack.interpolate(self.level()),
+      ex_flg: 0,
+      is_undead: 0,
+      special_skill_lv: 1,
     }
   }
 }
@@ -278,9 +310,9 @@ pub struct MemberActiveSkill<'a> {
 pub struct MemberStatsPrototype {
   pub hp: MinMaxRange,
   pub attack: MinMaxRange,
-  pub magic_attack: MinMaxRange,
+  pub attack_magic: MinMaxRange,
   pub defense: MinMaxRange,
-  pub magic_defense: MinMaxRange,
+  pub defense_magic: MinMaxRange,
   pub agility: MinMaxRange,
   pub dexterity: MinMaxRange,
   pub luck: MinMaxRange,
@@ -316,5 +348,11 @@ pub struct MinMaxRange {
 impl MinMaxRange {
   pub fn new(min: i32, max: i32) -> Self {
     Self { min, max }
+  }
+
+  pub fn interpolate(&self, level: i32) -> i32 {
+    const MAX_LEVEL: i32 = 60;
+    let ratio = (level - 1) as f32 / (MAX_LEVEL - 1) as f32;
+    (self.min as f32 + (self.max - self.min) as f32 * ratio).round() as i32
   }
 }

Cargo.toml
@@ -33,7 +33,7 @@ serde_repr = "0.1.20"
 config = "0.15.13"
 const-hex = "1.14.1"
 deadpool-postgres = { version = "0.14.1", features = ["serde"] }
-tokio-postgres = { version = "0.7.13", features = ["with-chrono-0_4"] }
+tokio-postgres = { version = "0.7.13", features = ["with-chrono-0_4", "with-serde_json-1"] }
 rustls = { version = "0.23.31" }
 tokio-postgres-rustls = "0.13.0"
 postgres-types = { version = "0.2.9", features = ["derive"] }

migrations/20251209000001_add_characters_and_members_table.sql
@@ -1,0 +1,60 @@
+-- Adds characters and members tables. Likely not final schema.
+
+drop table if exists user_characters cascade;
+create table user_characters
+(
+  user_id        bigint not null references users (id) on delete restrict,
+  character_id   bigint not null,
+  intimacy       int    not null default 0,
+  -- Skills, probably isn't worth normalizing
+  -- special_skills jsonb  not null,
+  primary key (user_id, character_id)
+);
+
+drop table if exists user_character_skills cascade;
+-- create table user_character_skills
+-- (
+--   user_id      bigint not null,
+--   character_id bigint not null,
+--   skill_id     bigint not null,
+--   level        int    not null default 1,
+--   primary key (user_id, character_id, skill_id),
+--   foreign key (user_id, character_id) references user_characters (user_id, character_id) on delete restrict
+-- );
+
+drop table if exists user_character_pieces cascade;
+-- create table user_character_pieces
+-- (
+--   user_id      bigint not null,
+--   character_id bigint not null,
+--   board_id     bigint not null,
+--   -- Current stage (blessing) of the board (blessing path)
+--   stage_id     bigint not null,
+--   primary key (user_id, character_id, board_id),
+--   foreign key (user_id, character_id) references user_characters (user_id, character_id) on delete restrict
+-- );
+
+drop table if exists user_members cascade;
+create table user_members
+(
+  user_id         bigint not null references users (id) on delete restrict,
+  member_id       bigint not null,
+  -- Leveling
+  xp              int    not null default 0,
+  promotion_level int    not null default 0,
+  -- Skills, probably isn't worth normalizing
+  -- active_skills   jsonb  not null,
+  primary key (user_id, member_id)
+);
+
+drop table if exists user_member_skills cascade;
+-- create table user_member_skills
+-- (
+--   user_id   bigint not null references users (id) on delete restrict,
+--   member_id bigint not null,
+--   skill_id  bigint not null,
+--   level     int    not null default 1,
+--   value     int    not null,
+--   primary key (user_id, member_id, skill_id),
+--   foreign key (user_id, member_id) references user_members (user_id, member_id) on delete restrict
+-- );

commit fd4e44a63b2f16eb0b4669b97bc905ad101785a7

char: Fix level calculation
f6b3b068 broke character affinity screen, this is fixes it.

src/api/mod.rs
@@ -315,6 +315,7 @@ pub struct SkillPaFameAddStatus {
 // See [Wonder_Data_CharacterParameter_Fields]
 #[derive(Debug, Serialize, Deserialize)]
 pub struct CharacterParameter {
+  /// Seems to be database-unique ID for user character
   pub id: i64,
   pub character_id: i64,
   pub rank: i32,

src/blob.rs
@@ -4,7 +4,7 @@ use crate::api::{
   CharacterParameter, MemberFameStats, MemberParameterWire, MemberStats, RemoteData, RemoteDataCommand,
   RemoteDataItemType, SpSkill,
 };
-use crate::level::get_member_level_calculator;
+use crate::level::get_intimacy_level_calculator;
 use crate::member::{Member, MemberActiveSkill, MemberPrototype, MemberStrength};
 use crate::user::session::Session;
 use anyhow::Context;
@@ -164,8 +164,6 @@ pub async fn run_login_migration(state: &AppState, session: &Session) {
 
 pub async fn get_login_remote_data(state: &AppState, session: &Session) -> Vec<RemoteData> {
   let masters = get_master_manager();
-  let characters = masters.get_master("character");
-  // let members = masters.get_master("member");
   let costumes = masters.get_master("costume");
   let backgrounds = masters.get_master("background");
 
@@ -250,31 +248,51 @@ pub async fn get_login_remote_data(state: &AppState, session: &Session) -> Vec<R
     })
     .collect::<Vec<_>>();
 
-  let characters = characters
-    .iter()
-    .enumerate()
-    .map(|(index, character)| {
-      AddCharacter::new(
-        index as i32,
-        CharacterParameter {
-          id: index as i64,
-          character_id: character.get("id").unwrap().as_str().unwrap().parse().unwrap(),
-          rank: 1,
-          rank_progress: 5,
-          sp_skill: vec![SpSkill {
-            group_id: 10000,
-            id: 100001,
-            lv: 2,
+  let characters = {
+    #[rustfmt::skip]
+    let statement = client
+      .prepare(/* language=postgresql */ r#"
+        select
+          character_id,
+          intimacy
+        from user_characters
+        where user_id = $1
+      "#)
+      .await
+      .context("failed to prepare statement").unwrap();
+    let rows = client
+      .query(&statement, &[&session.user_id])
+      .await
+      .context("failed to execute query")
+      .unwrap();
+    rows
+      .iter()
+      .map(|row| {
+        let character_id: i64 = row.get(0);
+        let intimacy: i32 = row.get(1);
+
+        AddCharacter::new(
+          character_id as i32,
+          CharacterParameter {
+            id: character_id,
+            character_id,
+            rank: intimacy,
+            rank_progress: get_intimacy_level_calculator().get_level(intimacy),
+            sp_skill: vec![SpSkill {
+              group_id: 10000,
+              id: 100001,
+              lv: 2,
+              is_trial: false,
+            }],
+            character_enhance_stage_id_list: vec![0, 0, 0, 0],
+            character_piece_board_stage_id_list: vec![100001001, 100002002, 100003003, 100004004],
             is_trial: false,
-          }],
-          character_enhance_stage_id_list: vec![0, 0, 0, 0],
-          character_piece_board_stage_id_list: vec![100001001, 100002002, 100003003, 100004004],
-          is_trial: false,
-        },
-      )
-      .into_remote_data()
-    })
-    .collect::<Vec<_>>();
+          },
+        )
+        .into_remote_data()
+      })
+      .collect::<Vec<_>>()
+  };
 
   let costumes = costumes
     .iter()

src/level/intimacy.rs
@@ -38,7 +38,8 @@ impl IntimacyLevelCalculator {
   pub fn get_level(&self, exp: i32) -> i32 {
     match self.absolute_xp_to_level.range(..=exp).next_back() {
       Some((_, &level)) => level,
-      None => 0,
+      // Level must be at least 1; 'intimacy_exp' starts from level 2, this branch is reachable
+      None => 1,
     }
   }
 

src/level/member.rs
@@ -84,8 +84,8 @@ impl MemberLevelCalculator {
 
     match level_to_absolute_xp_local.range(..=level).next_back() {
       Some((_, &xp)) => xp,
-      // 'member_lv_exp' starts from level 2, so this branch is actually reachable
-      None => 0,
+      // Level must be at least 1; 'member_lv_exp' starts from level 2, this branch is reachable
+      None => 1,
     }
   }
 